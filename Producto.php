ef7325f9f2275dddb23faf5d3ae33b74f4e831de
AlexD30 <104585822+YerkoD31@users.noreply.github.com>
2022-05-05 14:16:35 -0400

    Cambio extension programas

--- Producto.php
+++ Producto.php
@@ -7,10 +7,7 @@
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    
-
-
-    
+  
     <link rel="stylesheet" href="bootstrap.min.css">
     <link rel="stylesheet" href="bootstrap.min.js">
     <link rel="stylesheet" href="min.css">
@@ -19,68 +16,11 @@
     <title>Detalle de Productos--</title>
   </head>
 
+
   <body>
-    <nav class="navbar navbar-expand-sm navbar-dark bg-light">
-      <div class="container-fluid">
-  
-  
-          <img src="archivos iniciales/Fotos/FotosPagPrincipal/WhatsApp_Image_2022-04-07_at_4.59.49_PM-removebg-preview.png" alt="" width="100">
-          <h6><i>Flowers and Power </i></h6>           
-          <a  class="navbar-brand" >
-          </a>
-          <button 
-          class="navbar-toggler" 
-          type="button" 
-          data-bs-toggle="collapse" 
-          data-bs-target="#navbarSupportedContent" 
-          aria-controls="#navbarSupportedContent" 
-          aria-expanded="false" 
-          aria-label="Toggle navigation">
-  
-                  <span class="navbar-toggler-icon"></span>
-          </button> 
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="nav nav-tabs">
-            <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="index_home.html">Pagina Principal</a>
-            </li>
-            <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">Cuenta Usuario</a>
-                  <ul class="dropdown-menu">
-                    <li><a class="dropdown-item" href="InicioSesion.html">Acceder</a></li>
-                    <li><a class="dropdown-item" href="#" data-toggle="modal" data-target="#modaldesconnexion" id="modaldesconnexionb" >Desconectar Cuenta</a></li>
-                    <li><a class="dropdown-item" href="CrearCuenta.html">Crear Cuenta de Usuario</a></li>
-                    <li><a class="dropdown-item" href="Seguimiento.html">Seguimiento de Compra</a></li>
-                    <li><a class="dropdown-item" href="HistoricoCompra.html">Historico de Compras</a></li>
-                    
-                  </ul>
-            </li>
-            <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="Producto.html" role="button" aria-expanded="false">Producto</a>
-                  <ul class="dropdown-menu">
-                    <li><a class="dropdown-item" href="Producto.html">Ver Productos</a></li>
-                    <li><a class="dropdown-item" href="Categoria.html">Ver Categorias</a></li>
-                  </ul>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="Contacto.html">Contacto</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="Nosotros.html">Nosotros</a>
-            </li>
-        </ul>
-        
-      </div>
-      <form class="d-flex">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-primary" type="submit">Search</button>
-      </form>
-      <div id="compras">
-      <img src="archivos iniciales/Fotos/FotosPagPrincipal/carrito.jpg" alt="" width="50" height="50">           
-          <a id="comprastxt" class="navbar-brand" href="#"> <i>Compras </i>
-          </a>
-        </div>
-  </nav>   
+
+
+    <?php include("nav.php"); ?>
     
     <!--- Productos-->
     
@@ -383,9 +323,12 @@
   integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
   crossorigin="anonymous"></script>
 <script>
+
     $("#modaldesconnexionb").on('click',function(){
               $("#modaldesconnexion").modal('show');
           });
+
+    
 </script>
 
 </html>
